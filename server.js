f0cb8b04b4a1b3eed713e1ea5f2bd2c71f12132d
Yuhang Zhou <69432560+yuhang199@users.noreply.github.com>
2022-01-27 19:20:20 -0500

    ??

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 // Require http module
-const http = require('http')
+const http = require("http")
 // Require fs module
-const fs = require('fs')
+const fs = require("fs")
 // Require minimist module (make sure you install this one via npm).
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
-const args = require('minimist')(process.argv.slice(2))
+const args = require("minimist")(process.argv.slice(2))
 // Define allowed argument name 'port'.
-args['port']
+args["port"]
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
 const port = args.port || process.env.PORT || 3000
@@ -14,7 +14,7 @@ const port = args.port || process.env.PORT || 3000
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./www/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
-fs.readFile("./www/index.html", "utf-8", (err, data) =>{
+fs.readFile("./www/index.html", "utf-8", (err, data) => {
 // If there is an error, put it on the console error, return, and exit with error code 1. 
 // Do not be nice about exiting.
 if (err) {
